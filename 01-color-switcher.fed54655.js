03fbefb7634a23e6ad89104b7e2bde91776fa640
jrudenko <105347404+jrudenko@users.noreply.github.com>
2022-11-04 12:17:48 +0000

    Deploying to gh-pages from @ jrudenko/goit-js-hw-09@58fdd226e76a9af32475c6a46d1a971c61cb0a38 🚀

---
+++ 01-color-switcher.fed54655.js
@@ -0,0 +1,2 @@
+let t=t=>document.querySelector(t),e=null;t("[data-start]").addEventListener("click",(function(){console.log("clickStart"),t("[data-start]").setAttribute("disabled",!0),t("[data-stop]").removeAttribute("disabled"),e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t("[data-stop]").addEventListener("click",(function(){console.log("clickStop"),t("[data-stop]").setAttribute("disabled",!0),t("[data-start]").removeAttribute("disabled"),clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.fed54655.js.map
